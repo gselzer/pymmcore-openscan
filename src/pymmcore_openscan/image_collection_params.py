ade534a43f4634e87204943b1397b843e2b8af94
Gabriel Selzer <gjselzer@wisc.edu>
2025-05-23 17:07:58 -0500

    openscan params: Allow no device

--- src/pymmcore_openscan/image_collection_params.py
+++ src/pymmcore_openscan/image_collection_params.py
@@ -1,10 +1,11 @@
-from pymmcore_plus import CMMCorePlus
+from pymmcore_plus import CMMCorePlus, Device, DeviceProperty
 from qtpy.QtWidgets import (
     QComboBox,
     QFormLayout,
     QWidget,
 )
 from superqt import QLabeledDoubleSlider
+from superqt.utils import signals_blocked
 
 
 class ImageCollectionParameters(QWidget):
@@ -15,27 +16,13 @@ class ImageCollectionParameters(QWidget):
     ) -> None:
         super().__init__(parent)
         self._mmcore = mmcore or CMMCorePlus.instance()
-        self._dev = self._mmcore.getDeviceObject("OSc-LSM")
+        self._dev: Device | None = None
+        self._res_prop: DeviceProperty | None = None
 
         # -- Widgets --
-        self._res_prop = self._dev.getPropertyObject("LSM-Resolution")
         self._resolution = QComboBox()
-        for val in self._res_prop.allowedValues():
-            self._resolution.addItem(f"{val} x {val}")
-        starting_value = self._res_prop.value
-        self._resolution.setCurrentText(f"{starting_value} x {starting_value}")
-
-        self._zoom_prop = self._dev.getPropertyObject("LSM-ZoomFactor")
         self._zoom = QLabeledDoubleSlider()
-        self._zoom.setRange(self._zoom_prop.lowerLimit(), self._zoom_prop.upperLimit())
-        self._zoom.setValue(self._zoom_prop.value)
-
-        self._px_rate_prop = self._dev.getPropertyObject("LSM-PixelRateHz")
-        self._rates = sorted(self._px_rate_prop.allowedValues(), key=lambda x: float(x))
         self._px_rate = QComboBox()
-        for rate in self._rates:
-            self._px_rate.addItem(f"{float(rate) / 1e6} μs", rate)
-        self._px_rate.setCurrentText(f"{float(self._px_rate_prop.value) / 1e6} μs")
 
         # -- Layout --
         self._layout = QFormLayout(self)
@@ -48,15 +35,65 @@ class ImageCollectionParameters(QWidget):
         self._zoom.valueChanged.connect(self._update_zoom)
         self._px_rate.currentIndexChanged.connect(self._update_px_rate)
 
+        self._mmcore.events.systemConfigurationLoaded.connect(self._try_enable)
+        self._try_enable()
+
+    def _try_enable(self) -> None:
+        dev_present = "OSc-LSM" in self._mmcore.getLoadedDevices()
+
+        # Reset the component widgets
+        self._resolution.setEnabled(dev_present)
+        self._zoom.setEnabled(dev_present)
+        self._px_rate.setEnabled(dev_present)
+
+        with signals_blocked(self._resolution):
+            self._resolution.clear()
+        with signals_blocked(self._zoom):
+            self._zoom.setValue(1.0)
+        with signals_blocked(self._px_rate):
+            self._px_rate.clear()
+
+        # Done if device isn't present
+        if not dev_present:
+            self._dev = None
+            return
+
+        # Grab ref to device
+        self._dev = self._mmcore.getDeviceObject("OSc-LSM")
+        # Init resolution combo box
+        self._res_prop = self._dev.getPropertyObject("LSM-Resolution")
+        with signals_blocked(self._resolution):
+            for val in self._res_prop.allowedValues():
+                self._resolution.addItem(f"{val} x {val}")
+            starting_value = self._res_prop.value
+            self._resolution.setCurrentText(f"{starting_value} x {starting_value}")
+        # Init zoom slider
+        with signals_blocked(self._zoom):
+            zoom_prop = self._dev.getPropertyObject("LSM-ZoomFactor")
+            self._zoom.setRange(
+                int(zoom_prop.lowerLimit()), int(zoom_prop.upperLimit())
+            )
+            self._zoom.setValue(zoom_prop.value)
+        # Init pixel rate combo box
+        with signals_blocked(self._px_rate):
+            px_rate_prop = self._dev.getPropertyObject("LSM-PixelRateHz")
+            self._rates = sorted(px_rate_prop.allowedValues(), key=lambda x: float(x))
+            for rate in self._rates:
+                self._px_rate.addItem(f"{float(rate) / 1e6} μs", rate)
+            self._px_rate.setCurrentText(f"{float(px_rate_prop.value) / 1e6} μs")
+
     def _update_resolution(self, idx: int) -> None:
-        self._mmcore.setProperty(
-            self._dev.label, "LSM-Resolution", self._res_prop.allowedValues()[idx]
-        )
+        if self._dev is not None and self._res_prop is not None:
+            self._mmcore.setProperty(
+                self._dev.label, "LSM-Resolution", self._res_prop.allowedValues()[idx]
+            )
 
     def _update_zoom(self, value: float) -> None:
-        self._mmcore.setProperty(self._dev.label, "LSM-ZoomFactor", value)
+        if self._dev is not None:
+            self._mmcore.setProperty(self._dev.label, "LSM-ZoomFactor", value)
 
     def _update_px_rate(self, idx: int) -> None:
-        self._mmcore.setProperty(
-            self._dev.label, "LSM-PixelRateHz", self._px_rate.itemData(idx)
-        )
+        if self._dev is not None:
+            self._mmcore.setProperty(
+                self._dev.label, "LSM-PixelRateHz", self._px_rate.itemData(idx)
+            )
